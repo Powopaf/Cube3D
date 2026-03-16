93ad9c91eb4a3ec067bffe70ed74cfcc2d2f1fe9
Rehxco <salome.brchrd@gmail.com>
2026-03-16 18:46:01 +0100

    debut du parsing avec creation de la potentielle liste chainee que je vais utiliser pour le parsing et debut de fonction qui valide la map (recup de so long a voir si je peux l'utiliser)

---
+++ Src/Parser/valid_element.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   valid_element.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbrochar <sbrochar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/16 15:53:35 by sbrochar          #+#    #+#             */
+/*   Updated: 2026/03/16 18:37:33 by sbrochar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+
+void	store_map_info(char c, t_map *map, int x, int y)
+{
+	if (c == 'N' || c == 'W' || c == 'E' || c == 'S')
+	{
+		map->player_orientation = c;
+		map->position_player_x = x;
+		map->position_player_y = y;
+		map->player_count++;
+	}
+}
+
+bool	scan_map_elements(char **map_coordinates, t_map *map)
+{
+	int y;
+	int x;
+	char c;
+
+	y = 0;
+	while (y < map->map_height)
+	{
+		x = 0;
+		while (map_coordinates[y][x])
+		{
+			c = map_coordinates[y][x];
+			if (c != '0' && c != '1' && c != 'N' && c != 'S' && c != 'W'
+				&& c != ' ' && c != 'E' && c != '\n' && c != '\r')
+			{
+				write(2, "Parsing error.\n", 16);
+				return (false);
+			}
+			store_map_info(c, map, x, y);
+			x++;
+		}
+		y++;
+	}
+	return (true);
+}
---
+++ Src/Parser/valid_map.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   valid_map.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbrochar <sbrochar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/16 15:51:07 by sbrochar          #+#    #+#             */
+/*   Updated: 2026/03/16 18:12:27 by sbrochar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+
+bool	valid_extension(char *filename)
+{
+	int	len;
+
+	if (!filename)
+		return (false);
+	len = ft_strlen(filename);
+	if (len < 5)
+		return (false);
+	if (filename[len - 4] == '.' && filename[len - 3] == 'c' && filename[len
+			- 2] == 'u' && filename[len - 1] == 'b')
+		return (true);
+	return (false);
+}
