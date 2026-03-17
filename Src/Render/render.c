c822dcf882f8fc3d22acbc6b0390e53205e61a5c
Pierre-Antoine Fournier <pifourni@f4r11s14.clusters.42paris.fr>
2026-03-17 14:46:50 +0100

    init  render an know if it's wall ceiling or floor

--- Src/Render/render.c
+++ Src/Render/render.c
@@ -13,6 +13,11 @@
 * 0 = empty space
 * Player face north
 * player position: (x:2, y:3)
+*
+* CONST player fov: 60 degrees
+* CONST player height: 1 units 
+* CONST floor height: 0 units
+* CONST ceiling height: 2 units
 */
 
 typedef struct s_cordinate
@@ -21,16 +26,35 @@ typedef struct s_cordinate
 	int y;
 } t_cordinate;
 
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
 #define MAP_WIDTH 5
 #define MAP_HEIGHT 5
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+static void	pixel(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void render(t_data *img, char **map, t_cordinate player_pos, int fov);
 
 int	main()
 {
 	void	*mlx;
 	void	*win;
-	int		sizex;
-	int		sizey;
-	int fov = 110; // field of view in degrees
+	int fov = 60;
+	t_data img;
 	char *map[5] = {
 		"11111",
 		"11011",
@@ -43,9 +67,41 @@ int	main()
 	player_pos.x = 2;
 	player_pos.y = 3;
 	mlx = mlx_init();
-	mlx_get_screen_size(mlx, &sizex, &sizey); // get screen size
-	win = mlx_new_window(mlx, sizex / 2, sizey / 2, "RenderV1");
+	win = mlx_new_window(mlx, SCREEN_WIDTH, SCREEN_HEIGHT, "RenderV1");
+	img.img = mlx_new_image(mlx, SCREEN_WIDTH, SCREEN_HEIGHT);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	render(&img, map, player_pos, fov);
+	mlx_put_image_to_window(mlx, win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
 
+void draw_vertical_line(t_data *img, int x, int start, int end, int color)
+{
+	for (int y = start; y < end; y++)
+	{
+		pixel(img, x, y, color);
+	}
+}
 
+void draw_horizontal_line(t_data *img, int y, int start, int end, int color)
+{
+	for (int x = start; x < end; x++)
+	{
+		pixel(img, x, y, color);
+	}
+}
+
+void render(t_data *img, char **map, t_cordinate player_pos, int fov)
+{
+	int i = 0;
+	while (i < MAP_HEIGHT)
+	{
+		double ray_len = 6; // length of the ray from player to wall
+		int y_lo = (int)(SCREEN_HEIGHT / 2.0f - 1 / ray_len * SCREEN_HEIGHT / 2.0f); // calculate the top of the wall slice
+		int y_hi = (int)(SCREEN_HEIGHT / 2.0f + 1 / ray_len * SCREEN_HEIGHT / 2.0f); // calculate the bottom of the wall slice
+		draw_vertical_line(img, i, 0, y_lo - 1, 0xFF0000);
+		draw_vertical_line(img, i, y_lo, y_hi, 0x00FF00);
+		draw_vertical_line(img, i, y_hi + 1, SCREEN_HEIGHT - 1, 0x0000FF);
+		i++;
+	}
+}
